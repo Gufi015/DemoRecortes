cb14fcb418eb74ddbd06e93d6d7a15936a8ebb66
Rufino Rojas <lucras015@hotmail.com>
2018-11-14 18:35:52 -0600

    initial commit

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -157,8 +157,8 @@ function openPhotoGallery() {
 
 			intent.putExtra("outputX", 0);
 			intent.putExtra("outputY", 0);
-			//intent.putExtra("aspectX", 1);
-			//intent.putExtra("aspectY", 1);
+			intent.putExtra("aspectX", 0);
+			intent.putExtra("aspectY", 0);
 			intent.putExtra("scale", true);
 			intent.putExtra("return-data", true);
 
