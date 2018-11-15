9640ae952fd26eb028ab300e10384ec09b5eb047
Rufino Rojas <lucras015@hotmail.com>
2018-11-15 12:07:24 -0600

    se agrego metodo para recortar imagen cuando se tome una foto

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -8,12 +8,74 @@ var curBtn = Ti.UI.createButton({
 });
 
 var imageView = Ti.UI.createImageView({
-	width : 200,
-	height : 200,
+	width : '350dp',
+	height : '280dp ',
 	backgroundColor : '#ccc'
 });
 myView.add(curBtn);
 myView.add(imageView);
+
+var btnfoto = Ti.UI.createButton({
+	title : 'Foto',
+	left : 10,
+	bottom : 10,
+});
+$.index.add(btnfoto);
+
+btnfoto.addEventListener('click', function() {
+	if (!Ti.Media.hasCameraPermissions()) {
+		Ti.Media.requestCameraPermissions(function(e) {
+			if (e.success) {
+				//function
+				abrirCamara();
+			} else {
+				alert('no se puede optener permisos de la camara');
+			}
+		});
+	} else {
+		//funcion
+		abrirCamara();
+	}
+});
+
+function abrirCamara() {
+	Ti.Media.showCamera({
+		success : function(event) {
+			var intent = Ti.Android.createIntent({
+				action : "com.android.camera.action.CROP",
+				data : event.media.nativePath,
+				type : 'image/*'
+			});
+
+			//investigar esto
+			intent.putExtra("outputX", 300);
+			intent.putExtra("outputY", 300);
+			intent.putExtra("aspectX", 0);
+			intent.putExtra("aspectY", 0);
+			intent.putExtra("scale", true);
+			intent.putExtra("return-data", true);
+
+			var activity = $.index.getActivity();
+			activity.startActivityForResult(intent, function(param) {
+				if (param.resultCode == Ti.Android.RESULT_OK) {
+					if (param.intent) {
+						alert('Ok');
+						var imageData = param.intent.getBlobExtra("data");
+						imageView.image = imageData;
+						imageView.width = '350dp';
+						imageView.height = '280dp';
+
+					}
+				}
+			});
+		},
+		cancel : function(e) {
+		},
+		error : function(e) {
+			alert('ERROR IN ' + e.error);
+		}
+	});
+}
 
 curBtn.addEventListener('click', function(e) {
 	if (!Titanium.Filesystem.hasStoragePermissions()) {
@@ -37,12 +99,12 @@ function openPhotoGallery() {
 				data : event.media.nativePath,
 				type : 'image/*'
 			});
-			
+
 			//investigar esto
-			intent.putExtra("outputX", 200);
-			intent.putExtra("outputY", 200);
-			intent.putExtra("aspectX", 1);
-			intent.putExtra("aspectY", 1);
+			intent.putExtra("outputX", 300);
+			intent.putExtra("outputY", 300);
+			intent.putExtra("aspectX", 0);
+			intent.putExtra("aspectY", 0);
 			intent.putExtra("scale", true);
 			intent.putExtra("return-data", true);
 
@@ -53,6 +115,8 @@ function openPhotoGallery() {
 						alert('Ok');
 						var imageData = param.intent.getBlobExtra("data");
 						imageView.image = imageData;
+						imageView.width = '350dp';
+						imageView.height = '280dp';
 
 					}
 				}
@@ -67,5 +131,6 @@ function openPhotoGallery() {
 	});
 
 };
+
 $.index.add(myView);
 $.index.open();
